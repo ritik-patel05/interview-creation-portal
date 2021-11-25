23c8949af025d060e8c1fe420d91f2cea92b1783
unknown <ritikpatel05121999@gmail.com>
2021-11-26 01:12:20 +0530

    feat: add header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,37 @@
-const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import { Link } from "react-router-dom";
 
-export default Header
+const Header = () => {
+  return (
+    <header className="fixed w-full z-30">
+      <div className="max-w-6xl mx-auto px-5">
+        <div className="flex items-center justify-between h-16">
+          <div className="mr-4">
+            <Link to="/" className="cursor-pointer font-bold text-blue-500">
+              Interview Creation Portal
+            </Link>
+          </div>
+
+          <nav className="flex flex-grow justify-end">
+            <ul className="flex flex-wrap items-center">
+              <li>
+                <Link to="/upcoming" className="cursor-pointer mr-4">
+                  Upcoming Interviews
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/schedule"
+                  className="bg-blue-500 text-white cursor-pointer btn"
+                >
+                  Schedule Interview
+                </Link>{" "}
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
